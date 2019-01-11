b5c77c7df5e3f9b094c628368ff55bdfcb493511
Abilash Badri <abilash.badri@gmail.com>
2019-01-11 11:13:16 -0800

    Update syncvw.sh

--- syncvw.sh
+++ syncvw.sh
@@ -1,1 +1,1 @@
-rsync -r -a -v -e "ssh -l abadri" --exclude-from=/Users/abadri/rsyncscript/rsyncexclude $1 abadri-ld2.linkedin.biz:/home/abadri/workspace $2
+rsync -r -a -v -e "ssh -l abadri" --exclude-from=/Users/abadri/rsyncscript/rsyncexclude $1 abadri-ld2.xyz.com:/home/abadri/workspace $2
